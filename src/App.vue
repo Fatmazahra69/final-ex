46fcf8f89b590e9d4942b72cbbb9407e6e525cbb
unknown <fatma.marzouki69@hotmail.fr>
2021-12-13 09:19:59 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<template>
+    <h1 :style="{color:couleur}">Coucou</h1>
+    <TextInput msg="" />
+    <DateInput msg="" />
+    <TextArea msg="" />
+    <MenuSelect msg="" />
+    <MenuRadio msg="" />
+    <Button msg="" />
+    <bouttoncoul msg=""/>
+    <SearchAutocomplete msg=""/>
+    <ChangeColor msg="" v-on:validation="step='step2'"/>
+    <div v-if="step === 'step1'">
+      <h1>Etape:1</h1>
+    </div>
+    <div v-if="step === 'step2'">
+      <h1>Etape:2</h1>
+      <div>
+        <button>Vers Etape 3</button>
+      </div>
+    </div>
+    <div v-if="step === 'step3'">
+      <h1>Etape:3</h1>
+    </div>
+    <div id="app">
+    <SearchAutocomplete
+      :items="[
+        'Apple',
+        'Banana',
+        'Orange',
+        'Mango',
+        'Pear',
+        'Peach',
+        'Grape',
+        'Tangerine',
+        'Pineapple'
+      ]"
+    />
+  </div>
+
+</template>
+
+<script>
+import TextInput from "./components/TextInput.vue";
+import DateInput from "./components/DateInput.vue";
+import TextArea from "./components/TextArea.vue";
+import MenuSelect from "./components/MenuSelect.vue";
+import MenuRadio from "./components/MenuRadio.vue";
+import Button from "./components/Button.vue";
+import ChangeColor from "./components/ChangeColor.vue";
+import bouttoncoul from "./components/bouttoncoul.vue";
+import SearchAutocomplete from "./components/SearchAutocomplete.vue";
+export default {
+  name: "App",
+  components: {
+    TextInput,
+    DateInput,
+    TextArea,
+    MenuSelect,
+    MenuRadio,
+    Button,
+    ChangeColor,
+    bouttoncoul,
+    SearchAutocomplete,
+  },
+  data() {
+    return {
+      colorie: 'red',
+      step: 'step1',
+      couleur:'yellow'
+  }
+    }
+    
+  }
+
+
+</script>
+
+<style>
+.temp {
+  margin: 1em;
+}
+</style>
---
+++ src/components/bouttoncoul.vue
@@ -0,0 +1,56 @@
+<template>
+ <h1 :style="{ color:couleur }">Produits dans mon panier</h1>
+ <ul>
+ <li v-for="item in cart" v-bind:key={item}>{{item}}</li>
+ </ul>
+ 
+ <button v-on:click="$emit('enlarge-text')">Agrandir texte</button>
+ 
+ <button v-on:click="$emit('update-cart', cart)">Passer ma commande</button>
+
+ <button v-on:click="changerCouleur" >changer la couleur du titre</button>
+ <button v-on:click="couleur='pink'">changer coucou</button>
+ 
+</template>
+ 
+<script>
+export default {
+ name: 'HelloWorld',
+ data(){
+ return {
+ cart: ['pain', 'fromage', 'fleurs', 'vin'],
+couleur:'blue'
+ }
+ },
+ methods: {
+  changerCouleur() {
+    this.couleur = 'pink';
+  }
+ },
+ props: {
+ msg: String
+ }
+}
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+ margin: 40px 0 0;
+}
+ul {
+ padding: 0;
+}
+li {
+ margin: 0 10px;
+}
+a {
+ color: #42b983;
+}
+.button {
+  background-color: black;
+  padding: 1em;
+  color: white;
+  margin-right: 1em;
+}
+</style>
